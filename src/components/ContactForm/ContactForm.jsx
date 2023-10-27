0d20e617baa4c92b2b702b6625be1261c06bda44
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-10-27 18:56:34 +0200

    save contacts in localStorage

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,71 +3,79 @@ import PropTypes from 'prop-types';
 import styles from './ContactForm.module.css';
 
 class ContactForm extends Component {
-    static propTypes = {
-        onAddContact: PropTypes.func.isRequired,
-        contacts: PropTypes.array.isRequired,
-    };
+  static propTypes = {
+    onAddContact: PropTypes.func.isRequired,
+    contacts: PropTypes.array.isRequired,
+  };
 
-    state = {
-        name: '',
-        number: '',
-    };
+  state = {
+    name: '',
+    number: '',
+  };
 
-    handleInputChange = (e) => {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    };
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const { name, number } = this.state;
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
 
-        const isNameExist = this.props.contacts.some(
-            contact => contact.name.toLowerCase() === name.toLowerCase()
-        );
+    const isNameExist = this.props.contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
 
-        if (isNameExist) {
-            alert(`${name} is already in contacts.`);
-            return;
-        }
-
-        if (name && number) {
-            this.props.onAddContact(name, number);
-            this.setState({ name: '', number: '' });
-        }
-    };
-
-    render() {
-        const { name, number } = this.state;
-
-        return (
-            <form onSubmit={this.handleSubmit} className={styles.form}>
-                <label className={styles.label}>
-                    <p>Name</p>
-                    <input
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={this.handleInputChange}
-                        className={styles.input}
-                        required
-                    />
-                </label>
-                <label className={styles.label}>
-                    <p>Number</p>
-                    <input
-                        type="tel"
-                        name="number"
-                        value={number}
-                        onChange={this.handleInputChange}
-                        className={styles.input}
-                        required
-                    />
-                </label>
-                <button type="submit" className={styles.button}>Add contact</button>
-            </form>
-        );
+    if (isNameExist) {
+      alert(`${name} is already in contacts.`);
+      return;
     }
+
+    if (name && number) {
+      this.props.onAddContact(name, number);
+      this.setState({ name: '', number: '' });
+    }
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.contacts !== this.props.contacts) {
+      localStorage.setItem('Contacts', JSON.stringify(this.props.contacts));
+    }
+  }
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit} className={styles.form}>
+        <label className={styles.label}>
+          <p>Name</p>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleInputChange}
+            className={styles.input}
+            required
+          />
+        </label>
+        <label className={styles.label}>
+          <p>Number</p>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleInputChange}
+            className={styles.input}
+            required
+          />
+        </label>
+        <button type="submit" className={styles.button}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
 }
 
 export default ContactForm;
